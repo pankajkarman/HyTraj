f730c7080e264d45fc02827ef1efc0584996c1b3
pankajkarman <pankaj.kmr1990@gmail.com>
2021-07-27 09:20:12 +0530

    updated requirements.txt

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
      name='hytraj',
-     version='0.1.1',
+     version='0.1.2',
      description='Implementation of HySPLIT based trajectory modeling and analysis in python',
      long_description=long_description,
      long_description_content_type='text/markdown',
