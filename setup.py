36be055ebd9e6573e3ca84caa9433f4afca5932d
pankajkarman <pankaj.kmr1990@gmail.com>
2024-07-24 16:16:26 +0200

    updated python required to more than 3.6

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
      packages=find_packages(),
      py_modules=['hytraj'],
      install_requires=install_req,
-     python_requires="==3.8",
+     python_requires=">=3.6",
      setup_requires=['setuptools'],
 )
